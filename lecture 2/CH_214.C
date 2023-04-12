12911d1ffd1a682b5a28f0dbe92cad20645e7a1a
Jenish ghevariya <128150772+Jenish-Ghevariya@users.noreply.github.com>
2023-04-12 16:47:09 +0530

    Add files via upload

---
+++ lecture 2/CH_214.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+     int a=100;
+     int b=50;
+     int c;
+     clrscr();
+     c=a*b;
+     printf("the sum of a and b:%d",c);
+     getch();
+
+
+}
