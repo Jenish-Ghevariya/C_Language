e80636b3130db3342b6ba6f2b02f57ea91d950df
Jenish ghevariya <128150772+Jenish-Ghevariya@users.noreply.github.com>
2023-04-12 16:46:13 +0530

    Add files via upload

---
+++ lecture 1/CH_1_1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+    clrscr();
+    printf(" My name is jenish ghevariya\n\n\t");
+    printf(" My father name is bharatbhai\n\n");
+    printf(" My mother name is chetanaben\n\n\t");
+    printf(" My brother name is sahil\n\n");
+    printf(" My favourite dish is dhosa\n");
+    getch();
+
+}
